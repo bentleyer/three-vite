c769f8f8a4c7de5b496c4ddb7c57f7eb09bbd115
liukangning <liukangning@risenlighten.com>
2024-02-07 11:37:52 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <RouterLink to="/">Home</RouterLink> -->
+    <RouterLink to="/about">About</RouterLink>
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
